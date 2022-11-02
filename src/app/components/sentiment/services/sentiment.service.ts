8a61de2eba4c4d9962ede9fdb90649ab7e12f70d
SantosDylan <dylan.santos@epitech.eu>
2022-11-02 19:29:15 +0100

    fix sentiment api

--- src/app/components/sentiment/services/sentiment.service.ts
+++ src/app/components/sentiment/services/sentiment.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 import { combineLatest, map, Observable } from 'rxjs';
 import { Sentiment } from 'src/app/shared/interfaces/sentiment.interface';
 import { ApiService } from 'src/app/shared/services/api/api.service';
+
 
 @Injectable({ providedIn: 'root' })
 export class SentimentService {
   constructor(private api: ApiService) {}
 
   public getSentiment(symbol: string): Observable<Sentiment> {
-    return this.generateSentiment(symbol);
+    return this.generateSentiment(symbol, this.generateDateRange());
   }
 
-  private generateSentiment(symbol: string): Observable<Sentiment> {
-    return combineLatest({ symbolLookup: this.api.getSymbolLookup(symbol), insiderSentiment: this.api.getInsiderSentiment(symbol) }).pipe(
+  private generateSentiment(symbol: string, dateRange: { from: string; to: string }): Observable<Sentiment> {
+    return combineLatest({ symbolLookup: this.api.getSymbolLookup(symbol), insiderSentiment: this.api.getInsiderSentiment(symbol, dateRange) }).pipe(
       map(({ symbolLookup, insiderSentiment }) => ({
         symbol: symbol,
         name: symbolLookup.result[0].description,
@@ -20,4 +22,15 @@ export class SentimentService {
       }))
     );
   }
+
+  private generateDateRange(): { from: string; to: string } {
+    const today = new Date();
+    const hour = 60 * 60 * 1000;
+    const day = hour * 24;
+    const threeMonth = 3 * (day * 30);
+    const threeMonthAgo = today.getTime() - threeMonth;
+
+    return {from: moment(new Date(threeMonthAgo)).format('YYYY-MM-DD'), to: moment(new Date(today)).format('YYYY-MM-DD')}
+  }
+
 }
--- src/app/shared/services/api/api.service.ts
+++ src/app/shared/services/api/api.service.ts
@@ -6,20 +6,21 @@ import { InsiderSentiment } from '../../interfaces/insider-sentiment.interface';
 import { Quote } from '../../interfaces/quote.interface';
 import { SymbolLookup } from '../../interfaces/symbol-lookup.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ApiService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getSymbolLookup(symbol: string) : Observable<SymbolLookup> {
-        return this.http.get<SymbolLookup>(`${environment.api_url}/search?q=${symbol}&token=${environment.api_token}`)
-    }
+  getSymbolLookup(symbol: string): Observable<SymbolLookup> {
+    return this.http.get<SymbolLookup>(`${environment.api_url}/search?q=${symbol}&token=${environment.api_token}`);
+  }
 
-    getQuote(symbol: string) : Observable<Quote> {
-        return this.http.get<Quote>(`${environment.api_url}/quote?symbol=${symbol}&token=${environment.api_token}`)
-    }
-    
-    getInsiderSentiment(symbol: string) : Observable<InsiderSentiment> {
-        return this.http.get<InsiderSentiment>(`${environment.api_url}/stock/insider-sentiment?symbol=${symbol}&from=2015-01-01&to=2022-03-01&token=${environment.api_token}`)
-    }
-    
+  getQuote(symbol: string): Observable<Quote> {
+    return this.http.get<Quote>(`${environment.api_url}/quote?symbol=${symbol}&token=${environment.api_token}`);
+  }
+
+  getInsiderSentiment(symbol: string, dateRange: { from: string; to: string }): Observable<InsiderSentiment> {
+    return this.http.get<InsiderSentiment>(
+      `${environment.api_url}/stock/insider-sentiment?symbol=${symbol}&from=${dateRange.from}&to=${dateRange.to}&token=${environment.api_token}`
+    );
+  }
 }
