64fd0d5cefb1c7b83dde8daae17fec5037c0cb3e
SantosDylan <dylan.santos@epitech.eu>
2022-11-02 20:07:34 +0100

    stock list component cleaned

--- src/app/components/home/components/stocks-list/stocks-list.component.ts
+++ src/app/components/home/components/stocks-list/stocks-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Stock } from 'src/app/shared/interfaces/stock.interface';
 import { StocksListService } from './services/stocks-list/stocks-list.service';
@@ -7,12 +6,12 @@ import { StocksListService } from './services/stocks-list/stocks-list.service';
 @Component({
   selector: 'app-stocks-list',
   templateUrl: './stocks-list.component.html',
-  styleUrls: ['./stocks-list.component.scss']
+  styleUrls: ['./stocks-list.component.scss'],
 })
 export class StocksListComponent implements OnInit {
-  public stockList$ : Observable<Stock[]>
+  public stockList$: Observable<Stock[]>;
 
-  constructor(private stockList: StocksListService, private router: Router) {
+  constructor(private stockList: StocksListService) {
     this.stockList$ = this.stockList.getStockList$();
   }
 
@@ -23,5 +22,4 @@ export class StocksListComponent implements OnInit {
   public removeStock(stock: Stock) {
     this.stockList.removeStock(stock);
   }
-
 }
