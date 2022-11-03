c43d066a5762c02414b6a721874cfab1be6307f7
SantosDylan <dylan.santos@epitech.eu>
2022-11-03 10:43:34 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'https://finnhub.io/api/v1',
+  api_token: 'bu4f8kn48v6uehqi3cqg', 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   api_url: 'https://finnhub.io/api/v1',
   api_token: 'bu4f8kn48v6uehqi3cqg', 
-  
 };
 
 /*
